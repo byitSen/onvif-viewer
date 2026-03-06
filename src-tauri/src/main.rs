a023ef6c0518dcb582798946a0b3eed77236308a
林先森 <lin@linxiansendeMacBook-Pro.local>
2026-03-06 19:25:08 +0800

    Hide console window on Windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
+    #[cfg(windows)]
+    {
+        use windows_sys::Win32::UI::WindowsAndMessaging::{GetConsoleWindow, ShowWindow, SW_HIDE};
+
+        unsafe {
+            let console = GetConsoleWindow();
+            if console != 0 {
+                ShowWindow(console, SW_HIDE);
+            }
+        }
+    }
+
     onvif_viewer_lib::run();
 }
