bfc849bfe8f63575838d74420e43be09559cde74
林先森 <lin@linxiansendeMacBook-Pro.local>
2026-03-06 13:25:06 +0800

    Remove windows-sys console hide (incompatible version)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,3 @@
 fn main() {
-    #[cfg(windows)]
-    {
-        use windows_sys::Win32::UI::WindowsAndMessaging::{GetConsoleWindow, ShowWindow, SW_HIDE};
-
-        unsafe {
-            let console_window = GetConsoleWindow();
-            if console_window != 0 {
-                ShowWindow(console_window, SW_HIDE);
-            }
-        }
-    }
-
     onvif_viewer_lib::run();
 }
