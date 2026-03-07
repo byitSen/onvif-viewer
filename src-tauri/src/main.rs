7c5f09202ae2ddf6c13395ef502246bffdc50f17
林先森 <lin@linxiansendeMacBook-Pro.local>
2026-03-07 14:56:46 +0800

    Revert "Fix: use windows_subsystem to hide console (simpler approach)"
    This reverts commit 8d11da4518696de8b76ca23418ca13ca9907b0ae.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,19 @@
 #![cfg_attr(all(target_os = "windows"), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        use windows_sys::Win32::Foundation::HWND;
+        use windows_sys::Win32::UI::WindowsAndMessaging::{ShowWindow, SW_HIDE};
+
+        unsafe {
+            let console_window: HWND =
+                windows_sys::Win32::UI::WindowsAndMessaging::GetConsoleWindow();
+            if console_window != 0 {
+                ShowWindow(console_window, SW_HIDE);
+            }
+        }
+    }
+
     onvif_viewer_lib::run();
 }
